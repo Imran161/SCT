fd1391405a884f9e694301d4788cf0d072005c6f
Im89139073553 <i.nasyrov1@g.nsu.ru>
2024-06-19 08:52:58 +0000

    Эксперименты

--- train_sinusite.py
+++ train_sinusite.py
@@ -639,7 +639,7 @@ if __name__ == "__main__":
     print(torch.cuda.get_device_name(torch.cuda.current_device()))
 
     model = smp.FPN(
-        encoder_name="resnet50",
+        encoder_name="timm-gernet_m",
         encoder_weights="imagenet",
         in_channels=1,
         classes=num_classes,
@@ -654,7 +654,7 @@ if __name__ == "__main__":
     use_class_weight = True
     use_pixel_weight = True
     use_pixel_opt = True
-    power = "2.3_sinusite_weak"
+    power = "2.10_sinusite_weak"
 
     exp_setup = ExperimentSetup(
         train_loader, total_train, pixel_total_train, batch_size, num_classes
